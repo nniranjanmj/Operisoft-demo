a561bfa8c6e160c93f1c7f7e474d6dd1fd4c702e
Maulik Parmar <maulik.parmar@operisoft.com>
2023-08-02 11:41:10 +0530

    the files are ready

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Ensure script directory is set
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Make the script executable
+chmod +x "$SCRIPT_DIR/nginx.sh"
+
+# Update package lists
 sudo apt-get update
+
+# Install Nginx
 sudo apt-get install -y nginx
